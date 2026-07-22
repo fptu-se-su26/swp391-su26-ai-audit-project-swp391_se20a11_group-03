e3eb142df7cef4f21e9baf0a42bbcf32d6347fa2
Trần Văn Đức <ductrancode@gmail.com>
2026-07-22 09:07:29 +0700

    update del file old

--- Hoang_Xuan_Anh_Tuan/backend/src/main/java/com/hoangxuananhtuan/auction/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hoangxuananhtuan.auction.repository;
-
-import com.hoangxuananhtuan.auction.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- Hoang_Xuan_Anh_Tuan/backend/src/main/java/com/hoangxuananhtuan/auction/service/AuctionService.java
+++
@@ -1,7 +0,0 @@
-package com.hoangxuananhtuan.auction.service;
-
-import com.hoangxuananhtuan.auction.dto.AuctionEligibilityResponse;
-
-public interface AuctionService {
-    AuctionEligibilityResponse getEligibility(Long auctionId, Long userId);
-}
